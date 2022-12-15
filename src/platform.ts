cd0dfc5d276d50cf507734076cd4f42cf417257d
cupshir <christopher.upshir@gmail.com>
2022-12-15 09:56:37 -0700

    Work in progress

--- src/platform.ts
+++ src/platform.ts
@@ -3,6 +3,7 @@ import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, 
 import { PLATFORM_NAME, PLUGIN_NAME, ACCESSORY_TYPE, ACCESSORIES } from './settings';
 import { Light } from './light';
 import { Switch } from './switch';
+import { access } from 'fs';
 
 /**
  * HomebridgePlatform
@@ -17,7 +18,10 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
         public readonly log: Logger,
         public readonly config: PlatformConfig,
         public readonly api: API) {
-        this.log.debug('Finished initializing platform:', this.config.name);
+
+        this.log.debug('Test1: ' + config.exclude_accessories);
+        this.log.debug('Test2: ' + config.bridge_ip_address);
+        this.log.debug('Test3: ' + config.name);
         
         this.api.on('didFinishLaunching', () => {
             this.discoverDevices();
@@ -28,18 +32,42 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
         this.log.info('Loading accessory from cache:', accessory.displayName);
 
         if (!this.accessories.some(a => a.UUID === accessory.UUID)) {
+
             this.accessories.push(accessory);
         }
     }
 
     discoverDevices() {
+
+        let excludedAccessories = this.config.exclude_accessories;
+        let excludeAccessory = false;
+
+        console.log('excludedAccessories: ' + this.config.exclude_accessories);
+
+
         for (const accessory of ACCESSORIES) {
+            console.log('current accessory: ' + accessory.NAME);            
+            if (excludedAccessories.includes(accessory.NAME)){
+                console.log('exclude current accessory: ' + accessory.NAME);  
+                excludeAccessory = true;
+            }
+
             // generate a unique id
             const uuid = this.api.hap.uuid.generate((PLATFORM_NAME + accessory.NAME + accessory.TYPE));
 
             const existingAccessory = this.accessories.find(a => a.UUID === uuid);
 
             if (existingAccessory) {
+                //
+                if (excludeAccessory) {
+                    console.log('exclude existing accessory: ' + accessory.NAME);  
+
+                    this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME,[existingAccessory]);
+
+                    continue;
+                }
+
+
                 // the accessory already exists
                 this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
@@ -59,6 +87,12 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
                         break;
                 }
             } else {
+                if (excludeAccessory) {
+                    console.log('exclude new accessory: ' + accessory.NAME);  
+
+                    continue;
+                }
+
                 // the accessory does not yet exist, so we need to create it
                 this.log.info('Adding new accessory:', accessory.NAME);
         

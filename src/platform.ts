43f5068903080358db0db71fe2b8df962735b5e7
Christopher Upshir <christopher.upshir@gmail.com>
2022-12-16 13:07:26 -0700

    Add exclude functionality (#5)
    * Completed changes for new exclude accessory feature. Also changed debugging messages to make it easier to read what is going on.
    
    config.schema.json	.
    -added new exclude_accessories section
    
    light.ts src
    -refactored debug logging
    -added functionality to track when toggle state is in progress and updated api calls to be skipped if a toggle state is in progress
    
    platform.ts	src
    -refactored debug logging
    -added functionality that excludes accessories based on configured settings
    
    switch.ts	src
    -refactored debug logging
    -added functionality to track when toggle state is in progress and updated api calls to be skipped if a toggle state is in progress
    
    util.ts	src
    -removed unneeded console.log's
    -added device key index and process key num to debug messages to better track which device the messages apply to

--- src/platform.ts
+++ src/platform.ts
@@ -17,35 +17,44 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
         public readonly log: Logger,
         public readonly config: PlatformConfig,
         public readonly api: API) {
-        this.log.debug('Finished initializing platform:', this.config.name);
         
         this.api.on('didFinishLaunching', () => {
-            this.discoverDevices();
+            this.discoverAccessories();
         });
     }
 
     configureAccessory(accessory: PlatformAccessory) {
-        this.log.info('Loading accessory from cache:', accessory.displayName);
-
         if (!this.accessories.some(a => a.UUID === accessory.UUID)) {
             this.accessories.push(accessory);
         }
     }
 
-    discoverDevices() {
+    discoverAccessories() {
         for (const accessory of ACCESSORIES) {
-            // generate a unique id
-            const uuid = this.api.hap.uuid.generate((PLATFORM_NAME + accessory.NAME + accessory.TYPE));
+            this.log.debug('---------------------------');
+            this.log.debug(`${accessory.NAME} discover started.`);
 
+            let excludeAccessory = false;            
+            if (this.config.exclude_accessories && this.config.exclude_accessories.includes(accessory.NAME)) {
+                excludeAccessory = true;
+            }
+
+            const uuid = this.api.hap.uuid.generate((PLATFORM_NAME + accessory.NAME + accessory.TYPE));
             const existingAccessory = this.accessories.find(a => a.UUID === uuid);
 
             if (existingAccessory) {
-                // the accessory already exists
-                this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+                // if an accessory was included previously, but now excluded
+                // we need to remove it from the platform
+                if (excludeAccessory) {
+                    this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME,[existingAccessory]);
 
-                // add the accessory info to context
-                existingAccessory.context.device = accessory;
-        
+                    this.log.debug(`${accessory.NAME} excluded and unregistered.`);
+
+                    // skip to next accessory
+                    continue;
+                }
+
+                existingAccessory.context.device = accessory;        
                 this.api.updatePlatformAccessories([existingAccessory]);
 
                 switch (accessory.TYPE) {
@@ -58,14 +67,17 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
                     default:
                         break;
                 }
-            } else {
-                // the accessory does not yet exist, so we need to create it
-                this.log.info('Adding new accessory:', accessory.NAME);
-        
-                // create a new accessory
-                const newAccessory = new this.api.platformAccessory(accessory.NAME, uuid);
 
-                // add the accessory info to context
+                this.log.debug(`${existingAccessory.displayName} restored from cache.`);
+            } else {
+                if (excludeAccessory) {
+                    this.log.debug(`${accessory.NAME} excluded.`);
+
+                    // skip to next accessory
+                    continue;
+                }
+        
+                const newAccessory = new this.api.platformAccessory(accessory.NAME, uuid);
                 newAccessory.context.device = accessory;
 
                 switch (accessory.TYPE) {
@@ -80,6 +92,8 @@ export class AquaConnectLitePlatform implements DynamicPlatformPlugin {
                 }
         
                 this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [newAccessory]);
+
+                this.log.debug(`${accessory.NAME} added.`);
             }  
         }            
     }
